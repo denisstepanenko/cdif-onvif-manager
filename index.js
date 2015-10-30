ba6d33e9136f1bb161b3b71c6e91eeab02a8de66
out4b <seeds.c@gmail.com>
2015-10-30 22:10:37 +0800

    initial support of media / ptz action calls

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var util = require('util');
 var events = require('events');
 var onvif = require('onvif');
-var arp = require('node-arp');
-var CdifDevice = require('cdif-device');
-var spec = require('./onvif.json');
-
-
+var OnvifDevice = require('./lib/onvif-device')
 
 function OnvifManager() {
   this.discoverState = 'stopped';
@@ -29,49 +25,5 @@ OnvifManager.prototype.discoverDevices = function() {
 OnvifManager.prototype.stopDiscoverDevices = function() {
   this.discoverState = 'stopped';
 };
-
-function OnvifDevice(cam) {
-  this.cam = cam;
-  var spec = require('./onvif.json');
-  CdifDevice.call(this, spec);
-
-  // add actions
-  // launch rtsp-ffmpeg on getStreamUri call
-
-  this._connect = function(user, pass, callback) {
-    this.cam.username = user;
-    this.cam.password = pass;
-
-    var _this = this;
-    this.cam.connect(function(err) {
-      if (err) {
-        if(err.message === 'ONVIF SOAP Fault: Sender not Authorized') {
-          callback(new Error('auth failed'));
-        } else {
-          callback(new Error('cannot connect to cam'));
-        }
-      } else {
-        console.log(_this);
-        callback(null);
-      }
-    });
-  };
-
-  this._disconnect = function(callback) {
-    callback(null);
-  };
-
-  this._getHWAddress = function(callback) {
-    arp.getMAC(this.cam.hostname, function(err, mac) {
-    if (err) {
-        callback(new Error('hw address not found'), null);
-      } else {
-        callback(null, mac);
-      }
-    });
-  };
-}
-
-util.inherits(OnvifDevice, CdifDevice);
 
 module.exports = OnvifManager;
